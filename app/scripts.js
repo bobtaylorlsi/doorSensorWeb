8dc9daa06b7bf03c4a86d0878889cd308b56bc8f
Bob Taylor <20726157+bobtaylorlsi@users.noreply.github.com>
2020-01-14 10:27:46 -0500

    Updated API

--- app/scripts.js
+++ app/scripts.js
@@ -10,7 +10,7 @@ var request = new XMLHttpRequest();
 main();
 
 async function main() {
-  request.open('GET', 'https://8mfdub98va.execute-api.us-east-1.amazonaws.com/dev', true);
+  request.open('GET', 'https://laa1tvf1ej.execute-api.us-east-1.amazonaws.com/Dev', true);
   request.onload = async function () {
 
     // Begin accessing JSON data here
